93bbe8d1529802990c2624b2ab0cbc88fae675f7
ataulweb <ataulweb@gmail.com>
2023-08-19 12:57:51 +0600

    input field added

---
+++ input.js
@@ -0,0 +1,8 @@
+document.getElementById('button-addon1').addEventListener('click', function(){
+    const inputField = document.getElementById('input-field')
+    inputText = inputField.value;
+    
+    const defaultText = document.getElementById('default-text')
+    defaultText.innerText = inputText;
+    inputField.value =''
+})
